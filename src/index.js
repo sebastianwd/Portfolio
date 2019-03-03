03410c67d436a0a9e183c51f83ff43996ce0200a
Sebastian <sebastian.luque.pl@gmail.com>
2019-03-03 13:31:47 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require("normalize.css/normalize.css");
+require("./styles/index.scss");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pluginsTriggerElement = document.getElementById("plugins-trigger");
+    const pluginsElement = document.getElementById("plugins");
+
+    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+
+    pluginsTriggerElement.onclick = () => {
+        pluginsElement.classList.toggle(pluginsVisibleClass);
+    };
+    setTimeout(() => {
+        alert("hello");
+    }, 1000);
+});
